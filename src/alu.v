baf58937139314c0b3bcc76cb3074d0c79d249e6
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-10-21 14:29:07 +0800

    fix:fix the logic of alu.

--- src/alu.v
+++ src/alu.v
@@ -2,12 +2,12 @@ module ALU(
         input wire[31:0] value_1,
         input wire[31:0] value_2,
         input wire[3:0] op,
+        input wire[2:0] des_input,
         input wire clk,
         output reg[2:0] des,
-        output reg[31:0] result,
-        output reg busy
+        output reg[31:0] result
     );
-
+    reg[31:0] tmp;
     localparam [3:0]
                ADD = 4'b0000,
                AND = 4'b0001,
@@ -23,42 +23,42 @@ module ALU(
                GE  = 4'b1011,
                NEQ = 4'b1100,
                GEU = 4'b1101;
-    always @(*) begin
-        if (!busy) begin
-            case(op)
-                ADD:
-                    result <= value_1 + value_2;
-                AND:
-                    result <= value_1 & value_2;
-                OR:
-                    result <= value_1 | value_2;
-                SLL:
-                    result <= value_1 << value_2[4:0];
-                SRL:
-                    result <= value_1 >> value_2[4:0];
-                LT:
-                    result <= (value_1 < value_2) ? 1 : 0;
-                LTU:
-                    result <= ($unsigned(value_1) < $unsigned(value_2)) ? 1 : 0;
-                SRA:
-                    result <= value_1 >>> value_2[4:0];
-                SUB:
-                    result <= value_1 - value_2;
-                XOR:
-                    result <= value_1 ^ value_2;
-                EQ:
-                    result <= (value_1 == value_2) ? 1 : 0;
-                GE:
-                    result <= (value_1 >= value_2) ? 1 : 0;
-                NEQ:
-                    result <= (value_1 != value_2) ? 1 : 0;
-                GEU:
-                    result <= ($unsigned(value_1) >= $unsigned(value_2)) ? 1 : 0;
-                default:
-                    result <= 32'b0;
-            endcase
-            busy <= 1;
-        end
+    always @(posedge clk) begin
+        case(op)
+            ADD:
+                tmp <= value_1 + value_2;
+            AND:
+                tmp <= value_1 & value_2;
+            OR:
+                tmp <= value_1 | value_2;
+            SLL:
+                tmp <= value_1 << value_2[4:0];
+            SRL:
+                tmp <= value_1 >> value_2[4:0];
+            LT:
+                tmp <= (value_1 < value_2) ? 1 : 0;
+            LTU:
+                tmp <= ($unsigned(value_1) < $unsigned(value_2)) ? 1 : 0;
+            SRA:
+                tmp <= value_1 >>> value_2[4:0];
+            SUB:
+                tmp <= value_1 - value_2;
+            XOR:
+                tmp <= value_1 ^ value_2;
+            EQ:
+                tmp <= (value_1 == value_2) ? 1 : 0;
+            GE:
+                tmp <= (value_1 >= value_2) ? 1 : 0;
+            NEQ:
+                tmp <= (value_1 != value_2) ? 1 : 0;
+            GEU:
+                tmp <= ($unsigned(value_1) >= $unsigned(value_2)) ? 1 : 0;
+            default:
+                tmp <= 32'b0;
+        endcase
     end
-
+    always @(negedge clk) begin
+        des <= des_input;
+        result <= tmp;
+    end
 endmodule
