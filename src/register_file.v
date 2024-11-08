f7e9ba5b7f4292a47dbb9cbe41a72fe050aa30b9
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-11-09 07:49:13 +0800

    fix:fix the wrong output of RF.

--- src/register_file.v
+++ src/register_file.v
@@ -15,63 +15,72 @@ module RF(
         output reg [2:0] query1,
         output reg [2:0] query2
     );
-    reg busy;
-    reg [2:0] last_num;
-    reg [31:0] last_reg;
+    reg [31:0]check;
     reg [2:0]dependency[31:0];
     reg [31:0]regs[31:0];
+    reg [4:0] rs1_tmp;
+    reg [4:0] rs2_tmp;
+    reg [4:0] rd_tmp;
+    reg [2:0] dependency_tmp;
+    reg [31:0] value1_tmp;
+    reg [31:0] value2_tmp;
+    reg [2:0] query1_tmp;
+    reg [2:0] query2_tmp;
     integer cnt;
     initial begin
         query1 = 0;
         query2 = 0;
-        busy = 0;
-        last_num = 0;
-        last_reg = 0;
         for(cnt = 0; cnt < 32; cnt = cnt + 1) begin
             dependency[cnt] = 0;
             regs[cnt] = 0;
         end
     end
     always@(posedge clk) begin
+        check = regs[2];
+        rs1_tmp = rs1;
+        rs2_tmp = rs2;
+        rd_tmp = rd;
+        dependency_tmp = dependency_num;
         if(!rst) begin
-            if(busy && (last_reg != 0)) begin
-                dependency[last_reg] = dependency_num;
-                busy = 0;
+            if(instruction) begin
+                dependency[rd] <= dependency_num;
             end
             if(commit) begin
                 if(dependency[reg_num] == num_in) begin
-                    regs[reg_num] = 0;
+                    dependency[reg_num] = 0;
                 end
                 regs[reg_num] = data_in;
+            end
+        end
+        if(!rst) begin
+            if(instruction) begin
+                if(dependency[rs1_tmp] == 0) begin
+                    value1_tmp = regs[rs1_tmp];
+                    query1_tmp = 0;
+                end
+                else begin
+                    query1_tmp = dependency[rs1_tmp];
+                end
+                if(dependency[rs2_tmp] == 0) begin
+                    value2_tmp = regs[rs2_tmp];
+                    query2_tmp = 0;
+                end
+                else begin
+                    query2_tmp = dependency[rs2_tmp];
+                end
+            end
+        end
+        else begin
+            for(i = 0; i < 32; i = i + 1) begin
+                dependency[i] = 0;
             end
         end
     end
     integer i;
     always@(negedge clk) begin
-        if(!rst) begin
-        if(instruction) begin
-            if(dependency[rs1] == 0) begin
-                value1 <= regs[rs1];
-                query1 <= 0;
-            end
-            else begin
-                query1 <= dependency[rs1];
-            end
-            if(dependency[rs2] == 0) begin
-                value2 <= regs[rs2];
-                query2 <= 0;
-            end
-            else begin
-                query2 <= dependency[rs2];
-            end
-            busy<= 1;
-            last_num <= dependency_num;
-            last_reg <= rd;
-        end
-        end else begin
-            for(i = 0; i < 32; i = i + 1) begin
-                dependency[i] = 0;
-            end
-        end
+        value1 <= value1_tmp;
+        value2 <= value2_tmp;
+        query1 <= query1_tmp;
+        query2 <= query2_tmp;
     end
 endmodule
